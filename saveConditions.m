aef68a452eb5868c1f277363d30ffc7ce1868cfb
Nathan Ng <nathanng@MacBook-Pro.local>
2017-05-18 13:08:03 -0400

    The saveConditions.m file script will take all global variables and save them into a mostRecentFile.txt which can be copied into a matlab script and run to repeat a test. Random parts are not repeated.

---
+++ saveConditions.m
@@ -0,0 +1,28 @@
+function saveConditions
+
+file = fopen('mostRecentTest.txt', 'w+');
+if ~file
+    disp('Create file ''mostRecentTest.txt''.');
+end
+
+globalVars = who('global');
+
+for i = 1:length(globalVars)
+    varname = genvarname(globalVars{i});
+    eval(sprintf('global %s', varname));
+    var = eval(varname);
+    if ~isnan(var)
+        var = num2str(var);
+    end
+    if strcmp(varname, 'list')
+        fprintf(file, '%%%s = %s\n', varname, var);
+    else
+        fprintf(file, 'global %s\n', varname);
+        fprintf(file, '%s = %s\n', varname, var);
+    end
+    
+end
+
+fclose(file);
+
+end
